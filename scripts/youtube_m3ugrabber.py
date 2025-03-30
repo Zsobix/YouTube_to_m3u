64dc01b22df033cecf20424071acfb01908fa261
Zsobix <index@zsobix.xyz>
2025-03-30 13:08:56 +0200

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -2,17 +2,6 @@ import requests
 import os
 import sys
 
-def grab():
-    response = requests.get('https://www.youtube.com/@euronewshu/live').text
-    if '.m3u8' not in response:
-        print('bad')
-        print(response)
-    else:
-        a=response.split('"')
-        for line in a:
-            if line.endswith("index.m3u8"):
-                print(line)
-
 print('#EXTM3U')
 print('#EXTINF:-1')
-grab()
+print(os.system("yt-dlp -g 'https://www.youtube.com/@euronewshu/live'"))
