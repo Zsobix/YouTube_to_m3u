5644a3c288364d21fe56982ac197f170570181ce
Zsobix <index@zsobix.xyz>
2025-03-30 12:19:20 +0200

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -46,7 +46,7 @@ def grab(url):
             tuner += 5
     print(f"{link[start : end]}")
 
-print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
+print('#EXTM3U')
 print(banner)
 #s = requests.Session()
 with open('../youtube_channel_info.txt') as f:
@@ -56,10 +56,6 @@ with open('../youtube_channel_info.txt') as f:
             continue
         if not line.startswith('https:'):
             line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
             print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
             grab(line)
