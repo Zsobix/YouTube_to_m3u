67deba47bca74b62bf9c07be26f57e78478f4558
Zsobix <index@zsobix.xyz>
2025-03-30 12:35:23 +0200

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -1,47 +1,17 @@
-#! /usr/bin/python3
-
-banner = r'''
-#########################################################################
-#      ____            _           _   __  __                           #
-#     |  _ \ _ __ ___ (_) ___  ___| |_|  \/  | ___   ___  ___  ___      #
-#     | |_) | '__/ _ \| |/ _ \/ __| __| |\/| |/ _ \ / _ \/ __|/ _ \     #
-#     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
-#     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
-#                   |__/                                                #
-#                                  >> https://github.com/benmoose39     #
-#########################################################################
-'''
-
 import requests
 import os
 import sys
 
-windows = False
-if 'win' in sys.platform:
-    windows = True
-
 def grab(url):
-    response = requests.get(url, timeout=15).text
+    response = requests.get('https://www.youtube.com/@euronewshu/live').text
     if '.m3u8' not in response:
-        raise TypeError
+        print('bad')
     else:
         a=response.split('"')
         for line in a:
-            if line.startswith("https://manifest.googlevideo.com/api/manifest/hls_variant/expire") and line.endswith("index.m3u8"):
+            if line.endswith("index.m3u8"):
                 print(line)
 
 print('#EXTM3U')
-#s = requests.Session()
-with open('../youtube_channel_info.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            print(f'\n#EXTINF:-1')
-        else:
-            grab(line)
-            
-if 'temp.txt' in os.listdir():
-    #os.system('rm temp.txt')
-    os.system('rm watch*')
+print('#EXTINF:-1')
+grab(url)
