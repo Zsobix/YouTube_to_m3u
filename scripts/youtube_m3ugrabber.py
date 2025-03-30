5c99c7c61c02625bf31c1cb90168a93a5e4dfcf4
Zsobix <index@zsobix.xyz>
2025-03-30 13:26:52 +0200

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -2,7 +2,7 @@ import requests
 import os
 import time
 
+a = os.system("yt-dlp -g 'https://www.youtube.com/@euronewshu/live' --cookies cookies.firefox-private.txt")
 print('#EXTM3U')
 print('#EXTINF:-1')
-time.sleep(1)
-os.system("yt-dlp -g 'https://www.youtube.com/@euronewshu/live' --cookies cookies.firefox-private.txt")
+print(a)
